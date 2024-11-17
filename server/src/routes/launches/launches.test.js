990b89f93d4b8cf48696e71aa6da3964d7b2f34c
Abdullah Sufyan Murad <abdullahmurad2002@gmail.com>
2024-11-18 01:40:32 +0400

    Installed Jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,18 @@
+describe("Test GET /launches", () => {
+    test("It should respond with 200 Success", () => {
+        const response = 200;
+        expect(response).toBe(200);
+    });
+});
+
+
+describe("Test POST /launches", () => {
+    test("It should respond with 200 Success", () => {
+        const response = 200;
+        expect(response).toBe(200);
+    });
+
+    test("It shpuld catch missing required properties", () => {});
+    test("It should catch invalid date properties", () => {});
+
+});
