8dfb0e7eea46013c3e17d78416b8cc3cc6d291b6
Abdullah Sufyan Murad <abdullahmurad2002@gmail.com>
2024-11-18 13:51:59 +0400

    Installed Supertest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,12 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe("Test GET /launches", () => {
-    test("It should respond with 200 Success", () => {
-        const response = 200;
-        expect(response).toBe(200);
+    test("It should respond with 200 Success", async () => {
+        const response = await request(app)
+        .get("/launches")
+        .expect("Content-type", "/json/")
+        .expect(200);
     });
 });
 
